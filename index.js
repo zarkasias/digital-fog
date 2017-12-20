cfa78d5b4cce77abf52b8c9144bd6d790a311876
David Chambers <david_ma_chambers@yahoo.com>
2017-12-19 19:33:58 -0500

    update to node server

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 4800;
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
 app.use(express.static('public'))
 
@@ -8,5 +13,18 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-var port = process.env.PORT || 4800;
-app.listen(port);
+// eventEmitter.on('logging', function(message) {
+//   io.emit('log_message', message);
+// });
+
+
+// Override console.log
+// var originConsoleLog = console.log;
+// console.log = function(data) {
+//   eventEmitter.emit('logging', data);
+//   originConsoleLog(data);
+// };
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
